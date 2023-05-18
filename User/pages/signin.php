c839de586c37bb60a8c62ad8d3e6ed6a30c7ddb5
Yohannees <yohannesgetachewerieso@gmail.com>
2023-05-18 16:04:23 +0300

    updated

--- User/pages/signin.php
+++ User/pages/signin.php
@@ -11,40 +11,40 @@
 
 <body>
 
-        <div class="container">
-            <h1>Sign In</h1>
-            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                <label for="name">Name:</label>
-                <input type="text" id="name" name="name" required>
+    <div class="container">
+        <h1>Sign In</h1>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
 
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
 
-                <input type="submit" value="Sign In">
-            </form>
-            <?php
-            // validate form submission
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $name = test_input($_POST["name"]);
-                $password = test_input($_POST["password"]);
+            <input type="submit" value="Sign In">
+        </form>
+        <?php
+        // validate form submission
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $name = test_input($_POST["name"]);
+            $password = test_input($_POST["password"]);
 
-                if ($name != "admin" || $password != "password") {
-                    echo "<p class='error'>Invalid name or password</p>";
-                } else {
-                    echo "<p class='success'>Welcome back, $name!</p>";
-                }
+            if ($name != "admin" || $password != "password") {
+                echo "<p class='error'>Invalid name or password</p>";
+            } else {
+                echo "<p class='success'>Welcome back, $name!</p>";
             }
+        }
 
-            // sanitize form data
-            function test_input($data)
-            {
-                $data = trim($data);
-                $data = stripslashes($data);
-                $data = htmlspecialchars($data);
-                return $data;
-            }
-            ?>
-        </div>
+        // sanitize form data
+        function test_input($data)
+        {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+        ?>
+    </div>
 
 </body>
 
